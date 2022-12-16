b6adcc46902d5c99f4455f93ce3d347216b71a76
Rajalingam66 <40492669+Rajalingam66@users.noreply.github.com>
2022-12-16 16:33:09 +0530

    fix: Eslint issues

--- client/src/components/ErrorBoundary.js
+++ client/src/components/ErrorBoundary.js
@@ -1,5 +1,6 @@
-import React from "react";
-import GlobalHeader from "../components/GlobalHeader";
+import React from 'react';
+import GlobalHeader from '../components/GlobalHeader';
+import PropTypes from 'prop-types';
 
 export default class ErrorBoundary extends React.Component {
   constructor(props) {
@@ -8,21 +9,26 @@ export default class ErrorBoundary extends React.Component {
   }
   componentDidCatch(error, info) {
     console.error(error);
+    console.error(info);
     //we can log the error into DB
   }
   static getDerivedStateFromError(error) {
     // Update state so the next render will show the fallback UI.
+    console.error(error);
     return { hasError: true };
   }
   render() {
     if (this.state.hasError) {
       return (
         <>
-        <GlobalHeader/>
-          <img src="Error_Img.webp" style={{margin: "0 20% 0 20%"}}></img>
+          <GlobalHeader />
+          <img src="Error_Img.webp" style={{ margin: '0 20% 0 20%' }}></img>
         </>
       );
     }
     return this.props.children;
   }
 }
+ErrorBoundary.propTypes = {
+  children: PropTypes.any
+};
