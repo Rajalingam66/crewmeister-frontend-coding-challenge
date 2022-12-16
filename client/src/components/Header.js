359d6e1836f4f307a6dd7597c2cbb1e965eb0266
Rajalingam66 <40492669+Rajalingam66@users.noreply.github.com>
2022-12-16 11:39:36 +0530

    feat: Absence count changes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,11 +1,32 @@
-const Header = () => {
-    return (
-        <div className="flex justify-content-between">
-          <span className="p-input-icon-left">
-            Total Count
-          </span>
-        </div>
-      );
-}
+import styled from "styled-components";
+
+const Title = styled.h4`
+  color: #ff9419;
+  width: 50%;
+`;
+const Records = styled.p`
+  width: 15%;
+  margin: 0;
+  background: #fefdfb;
+  padding: 0.2rem;
+  border: 1px solid #ff9419;
+  color: #ff9419;
+  font-weight: 600;
+`;
+const Count = styled.span`
+  font-size: 18px;
+  margin-left: 10px;
+`;
+const Header = (totalCount) => {
+  return (
+    <div className="flex justify-content-between">
+      <Title>Absence Manager</Title>
+      <Records>
+        <span>Absence count</span>
+        <Count>{totalCount} </Count>{" "}
+      </Records>
+    </div>
+  );
+};
 
 export default Header;
