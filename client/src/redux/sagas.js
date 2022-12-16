66084693e814636e65b74f3e8a172d7b9e392b92
Rajalingam66 <40492669+Rajalingam66@users.noreply.github.com>
2022-12-16 16:36:06 +0530

    fix: Eslint issues

--- client/src/redux/sagas.js
+++ client/src/redux/sagas.js
@@ -1,37 +1,34 @@
-import { takeLatest, all, put, } from 'redux-saga/effects';
+import { takeLatest, all, put } from 'redux-saga/effects';
 import * as API from './api';
 
-
-export function* getAbsenceDetails(){
-    //showLoader();
-    try{
-        const apiResponse = yield API.getAbsenceData();
-        let response = [];
-        // console.log("apiResponse",apiResponse);
-        if(apiResponse?.data?.data) {
-            response = apiResponse?.data?.data?.map((absence)=>{
-                    const absenceData = {...absence};
-                    absenceData.startDate = new Date(absenceData.startDate);
-                    absenceData.endDate = new Date(absenceData.endDate);
-                    return absenceData;
-                    });
-        }
-            
-        yield put({type:'UPDATE_ABSENCE_DETAILS', response: response});
-    }
-    catch(error)
-    {
-        console.log("getAbsenceDetails saga error",error);
-        yield put({type:'SHOW_ERROR_NOTIFICATION'});
-         //hideLoader();
+export function* getAbsenceDetails() {
+  //showLoader();
+  try {
+    const apiResponse = yield API.getAbsenceData();
+    let response = [];
+    // console.log("apiResponse",apiResponse);
+    if (apiResponse?.data?.data) {
+      response = apiResponse?.data?.data?.map((absence) => {
+        const absenceData = { ...absence };
+        absenceData.startDate = new Date(absenceData.startDate);
+        absenceData.endDate = new Date(absenceData.endDate);
+        return absenceData;
+      });
     }
 
+    yield put({ type: 'UPDATE_ABSENCE_DETAILS', response: response });
+  } catch (error) {
+    console.log('getAbsenceDetails saga error', error);
+    yield put({ type: 'SHOW_ERROR_NOTIFICATION' });
     //hideLoader();
+  }
+
+  //hideLoader();
 }
 
 export function* actionWatcher() {
-    yield takeLatest('GET_ABSENCE_DETAILS', getAbsenceDetails);
+  yield takeLatest('GET_ABSENCE_DETAILS', getAbsenceDetails);
 }
 export default function* rootSaga() {
-    yield all([actionWatcher()]);
-} 
+  yield all([actionWatcher()]);
+}
