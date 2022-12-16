3aed24307d82187a2aa94ce0e1f29f5061fe54ad
Rajalingam66 <40492669+Rajalingam66@users.noreply.github.com>
2022-12-16 09:57:14 +0530

    fix: Removed unwanted code

--- api/controllers/absenceController.js
+++ api/controllers/absenceController.js
@@ -1,25 +1,11 @@
-import { getAbsenceInfo } from "../models/absenceModel.js"
+import { getAbsenceInfo } from "../models/absenceModel.js";
 
-
-export const getAbsenceDetails = async(req, res) => {
-    try{
-    //     const pageSize = 10;
-    //     const absenceRequest = {
-    //     offset : (req.params.page - 1) * pageSize || 0,
-    //     limit : req.params.page * pageSize,
-    //     startDate : req.query.startDate,
-    //     endDate : req.query.endDate,
-    //     status : req.query.status,
-    //     type : req.query.type,
-    // }
+export const getAbsenceDetails = async (req, res) => {
+  try {
     const data = await getAbsenceInfo();
-
-    // const data = await getAbsenceInfo(absenceRequest);
     res.status(200).send(data);
-    
-    }
- catch (err) {
-   console.log("error",err);
-    res.status(500).send({errors: err});
-}
-}
+  } catch (err) {
+    console.log("error", err);
+    res.status(500).send({ errors: err });
+  }
+};
