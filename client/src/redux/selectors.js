da64b2cfaae242a0a52f662f8768f2eddd8ea1a9
Rajalingam66 <40492669+Rajalingam66@users.noreply.github.com>
2022-12-16 16:36:40 +0530

    fix: Eslint issues

--- client/src/redux/selectors.js
+++ client/src/redux/selectors.js
@@ -1,15 +1,13 @@
-import { createSelector} from 'reselect';
+import { createSelector } from 'reselect';
 export const selectState = (state) => {
-// console.log("state",state?.absence);
- return state?.absence || {};
-}
+  // console.log("state",state?.absence);
+  return state?.absence || {};
+};
 
-const absenceDetailsSelector = createSelector(selectState, (data) =>{
-const {absenceDetails , totalCount } = data;
-// console.log("Selector Data", data);
-return { absenceDetails, totalCount };
+const absenceDetailsSelector = createSelector(selectState, (data) => {
+  const { absenceDetails, totalCount } = data;
+  // console.log("Selector Data", data);
+  return { absenceDetails, totalCount };
 });
 
-export {
-    absenceDetailsSelector,
-};
+export { absenceDetailsSelector };
