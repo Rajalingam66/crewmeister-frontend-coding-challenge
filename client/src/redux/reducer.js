b8f0f45abf1394479aaf62a64c86009f4a88ec10
Rajalingam66 <40492669+Rajalingam66@users.noreply.github.com>
2022-12-16 16:35:33 +0530

    fix: Eslint issues

--- client/src/redux/reducer.js
+++ client/src/redux/reducer.js
@@ -1,32 +1,31 @@
 const initialState = {
-    absenceDetails : [],
-    isNotificationVisible: false,
-    totalCount: 0,
-    RESULTS_PER_PAGE: 10,
-}
+  absenceDetails: [],
+  isNotificationVisible: false,
+  totalCount: 0,
+  RESULTS_PER_PAGE: 10
+};
 
 export const absenceReducer = (state = initialState, action) => {
-switch(action.type) {
+  switch (action.type) {
     case 'UPDATE_ABSENCE_DETAILS':
-    // console.log("action.response",action.response);    
-    return {
-            ...state,
-            absenceDetails: action.response,
-            totalCount: action.response.length,
-        };
+      // console.log("action.response",action.response);
+      return {
+        ...state,
+        absenceDetails: action.response,
+        totalCount: action.response.length
+      };
     case 'SHOW_ERROR_NOTIFICATION':
-        return { 
-            ...state,
-            isNotificationVisible: true,
-        };
-        case 'HIDE_ERROR_NOTIFICATION':
-        return { 
-            ...state,
-            isNotificationVisible: false,
-        };
-       
-        default:
-            return state;
-}
+      return {
+        ...state,
+        isNotificationVisible: true
+      };
+    case 'HIDE_ERROR_NOTIFICATION':
+      return {
+        ...state,
+        isNotificationVisible: false
+      };
 
-}
+    default:
+      return state;
+  }
+};
